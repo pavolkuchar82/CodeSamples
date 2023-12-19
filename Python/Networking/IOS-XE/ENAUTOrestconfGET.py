5423586c5d5295060c68a359e7461b55d1445767
Pavol Kuchar <pavol.kuchar@outlook.com>
2023-12-19 15:26:53 +0000

    tet1

--- Python/Networking/IOS-XE/ENAUTOrestconfGET.py
+++ Python/Networking/IOS-XE/ENAUTOrestconfGET.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 router = {
-    "host": "ios-xe-mgmt.cisco.com",
-    "port": "9443",
-    "user": "root",
-    "password": "D_Vay!_10&"
+    "host": "10.10.20.48",
+    "port": "443",
+    "user": "developer",
+    "password": "C1sco12345"
 }
 
 headers = {
@@ -13,16 +13,16 @@ headers = {
     "Content-Type": "application/yang-data+json"
 }
 
-# url = f"https://{router['host']}:{router['port']}/restconf/data/ietf-routing:routing"
-
-# response = requests.get(url=url, headers=headers, auth=(
-#     router['user'], router['password']), verify=False).json()
-
-# print(json.dumps(response, indent=2))
-
-url = f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/"
+url = f"https://{router['host']}:{router['port']}/restconf/data/ietf-routing:routing"
 
 response = requests.get(url=url, headers=headers, auth=(
     router['user'], router['password']), verify=False).json()
 
 print(json.dumps(response, indent=2))
+
+# url = f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/"
+
+# response = requests.get(url=url, headers=headers, auth=(
+#     router['user'], router['password']), verify=False).json()
+
+# print(json.dumps(response, indent=2))
